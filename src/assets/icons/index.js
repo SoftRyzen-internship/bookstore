0e2a2cb4eb9c7a68380f68f0cb74528116cf5be6
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-03-30 16:56:49 +0300

    BS-3: Change logo icon name

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,4 @@
-import { ReactComponent as Logo } from 'assets/icons/svg/logo.svg';
+import { ReactComponent as Logo } from 'assets/icons/svg/book.svg';
 import { ReactComponent as Search } from 'assets/icons/svg/search.svg';
 import { ReactComponent as Call } from 'assets/icons/svg/call.svg';
 import { ReactComponent as User } from 'assets/icons/svg/user.svg';
