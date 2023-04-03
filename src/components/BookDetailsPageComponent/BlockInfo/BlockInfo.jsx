4966bdccf163582ea4c26be0931cbcb37be2934f
Kretsul-Vitaliy <turistvital1988@gmail.com>
2023-04-03 20:26:26 +0300

    BS:44-fix-bugs
    useSearchParams from react-router-dom

--- src/components/BookDetailsPageComponent/BlockInfo/BlockInfo.jsx
+++ src/components/BookDetailsPageComponent/BlockInfo/BlockInfo.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ICONS } from 'assets/icons';
 import s from './BlockInfo.module.scss';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { routesPath } from 'router/routesPath';
 import { deleteBook } from 'services/books-api';
 import { SpinnerButton } from 'components/SpinnerButton';
@@ -15,11 +15,11 @@ export const BlockInfo = ({ data }) => {
   const state = location?.state;
   const backPage = state?.from?.pathname + state?.from?.search;
   const count = state?.count;
+  const [searchParams] = useSearchParams();
 
   const decreasePathPage = path => {
-    const searchParams = new URLSearchParams(path.slice(path.indexOf('?')));
     const currentPage = parseInt(searchParams.get('page'));
-    searchParams.set('page', currentPage === 1 ? currentPage : currentPage - 1);
+    searchParams.set('page', Math.max(currentPage - 1, 1));
     return path.slice(0, path.indexOf('?')) + '?' + searchParams.toString();
   };
 
